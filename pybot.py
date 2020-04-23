71a885764be467563a0316780d7370fb0f2c253a
Aaron C <aaron.carroll.git@gmail.com>
2020-04-23 09:58:39 -0400

    99 and roll_dice commands

---
+++ pybot.py
@@ -0,0 +1,44 @@
+# bot.py
+import os
+import random
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORT_GUILD')
+
+client = discord.Client()
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 100 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt'
+        ),
+        'Ok, no hard feelings, but I hate you. Not jokeing. Bye.',
+        'Sarge, with all due respect, I am gonna completely ignore everything you just said.',
+        'Great, I\'d lke to see your 8-est dollar bottle of wine\, please.'
+    ]
+    
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+    
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    
+    ]
+    await ctx.send(', '.join(dice))
+    
+bot.run(TOKEN)
